9c53935db9d8918c995806f95d019b0472ecb1ac
J-hon <jonadify007@gmail.com>
2019-09-27 14:19:45 +0100

    Designed educate.blade.php

--- resources/views/educate.blade.php
+++ resources/views/educate.blade.php
@@ -5,33 +5,111 @@
 @section('content')
 
     <h3 class="text-center font">Welcome to the institute of learning...</h3>
+    <br>
 
-    <div class="row" id="row">
-        <div class="col-md-6 col-sm-12" id="bg">
-            <a href="{{ route('personal.index') }}" id="btn-lg" class="btn btn-lg btn-primary">
-                Personal Data <span style="float: right"><i class="fa fa-angle-double-right" aria-hidden="true"></i></span>
-            </a>
+    <div class="box-container">
+        <div class="box">
+            <div class="text-center">
+                <a href="{{ route('personal.index') }}">
+                    <i class="fas fa-user-alt fa-5x"></i><br>
+                    Personal Data
+                </a>
+            </div>
         </div>
-
-        <div class="col-md-6 col-sm-12" id="bg">
-            <a href="#" id="btn-lg" class="btn btn-lg btn-primary">
-                Transcript <span style="float: right"><i class="fa fa-angle-double-right" aria-hidden="true"></i></span>
-            </a>
+        <div class="box">
+            <div class="text-center">
+                <a href="#">
+                    <i class="fas fa-file fa-5x"></i><br>
+                    Transcript
+                </a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="text-center">
+                <a href="#">
+                    <i class="fas fa-money-check fa-5x"></i><br>
+                    Fees
+                </a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="text-center">
+                <a href="#">
+                    <i class="fas fa-receipt fa-5x"></i><br>
+                    Course Registration
+                </a>
+            </div>
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-6 col-sm-12" id="bg">
-            <a href="#" id="btn-lg" class="btn btn-lg btn-primary">
-                Fees <span style="float: right"><i class="fa fa-angle-double-right" aria-hidden="true"></i></span>
-            </a>
-        </div>
+    <style>
+        .box-container {
+            width: 100%;
+        }
 
-        <div class="col-md-6 col-sm-12" id="bg">
-            <a href="#" id="btn-lg" class="btn btn-lg btn-primary">
-                Course Registration <span style="float: right"><i class="fa fa-angle-double-right" aria-hidden="true"></i></span>
-            </a>
-        </div>
-    </div>
+        a {
+            text-decoration: none;
+            color: #A0A0A0;
+        }
+
+        a:hover {
+            text-decoration: none;
+            color: #282828;
+        }
+
+        .box-container .box {
+            padding: 80px 30px 80px 30px;
+            display: inline-block;
+            min-height: 45vh;
+            max-height: 45vh;
+            background-color: #f0f0f0;
+            width: 23%;
+            margin: 9px;
+            box-shadow: .5px .5px 4px .5px #C0C0C0;
+        }
+
+        .box-container .box:hover {
+            display: inline-block;
+            cursor: pointer;
+            box-shadow: 2px 4px 18px 2px #C0C0C0;
+        }
+
+        @media screen and (max-width: 600px) {
+            .box-container {
+                margin-left: 100px;
+                width: 100%;
+            }
+
+            .box-container .box {
+                padding: 75px;
+                display: inline-block;
+                min-height: 45vh;
+                max-height: 45vh;
+                background-color: #f0f0f0;
+                width: 50%;
+                margin: 10px;
+                box-shadow: .5px .5px 4px .5px #C0C0C0;
+            }
+        }
+
+        @media (min-width: 600px) and (max-width: 992px) {
+            .box-container {
+                margin-left: 40px;
+                width: 100%;
+            }
+
+            .box-container .box {
+                padding: 80px 30px 80px 30px;
+                display: inline-block;
+                min-height: 45vh;
+                max-height: 45vh;
+                background-color: #f0f0f0;
+                width: 40%;
+                margin: 10px;
+                box-shadow: .5px .5px 4px .5px #C0C0C0;
+            }
+        }
+
+    </style>
 
 @endsection
---
+++ resources/views/personal/edit.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: John
+ * Date: 9/27/2019
+ * Time: 12:03 PM
+ */
